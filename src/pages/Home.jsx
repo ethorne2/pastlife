3fa86d3bf26aa349b1e649eb802ab3db14982258
Elizabeth Thorne <66274014+ethorne2@users.noreply.github.com>
2022-04-22 21:19:06 -0500

    Loading - implement fetching API and getting data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import DefinitionCard from '../components/DefinitionCard';
- /* Date Picker imports */
-import { useState } from 'react';
+ /* Date Picker and Country Picker imports */
+import { useState, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-date-picker';
+import Select from 'react-select';
+import countryList from 'react-select-country-list';
+
 
 function Home() {
 
+    /* useState for birthdate */
     const [birthdate, setBirthdate] = useState(new Date());
+
+    /* useState for birth country */
+    const [country, setCountry] = useState('');
+    const options = useMemo(() => countryList().getData(), []);
+
+    const countryChangeHandler = country => {
+        setCountry(country);
+    };
 
     /* Get the values of the birthday */
     const formattedBirthdate = birthdate.toISOString().slice(0,10);
 
-    /* function to send formattedBirthdate to the loading screen for fetching from API */
+    /* function to send formattedBirthdate and country to the loading screen for fetching from API */
     const navigate = useNavigate();
     const sendtoLoading = () => {
-        navigate('/loading', {state:{formattedBirthdate}});
+        navigate('/loading', {state:{birthdate:formattedBirthdate, country:country}});
     };
+
 
     return (
         <div className='centered-home-screen'>
@@ -24,18 +37,20 @@ function Home() {
             <h1 className='page-title'>Welcome to Past Life</h1>
             <br></br>
             <DefinitionCard />
-            <br></br>
             <h2 className='instruction-title'>Instructions</h2>
             <p className='instruction-text'>Ready to find your past life?</p>
-            <p className='instruction-text'>Enter your birthdate and birth location 
-            to learn about your past soul.</p>
+            <p className='instruction-text'><b>Enter your birthdate and birth country 
+            to learn about your past life.</b></p>
             <br></br>
             <h2 className='instruction-title'> Enter your birthdate</h2>
-            <br></br>
             <div className='date-picker'>
                 <DatePicker onChange={setBirthdate} value={birthdate}/>
             </div>
-            <p>Your date = {formattedBirthdate} </p>
+            <br></br>
+            <h2 className='instruction-title'> Enter your birth Country</h2>
+            <div className='country-picker-container'>
+                <Select options={options} value={country} onChange={countryChangeHandler} />
+            </div>
             <div className='button-view-past-life'>
                 <button className='home-button' onClick={() => {sendtoLoading()}}>View My Past Life</button>
             </div>
--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { access_token, access_email } from '../WikiMediaAccess';
 
 function Loading() {
 
     const location = useLocation();
-    const Birthdate = location.state.formattedBirthdate;
-    const BirthdateYear = Birthdate.slice(0,4);
-    const BirthdateMonth = Birthdate.slice(5,7);
-    const BirthdateDay = Birthdate.slice(8,11);
+    const birthdate = location.state.birthdate;
+    const birthdateYear = birthdate.slice(0,4);
+    const birthdateYearInt = parseInt(birthdateYear);
+    const birthdateMonth = birthdate.slice(5,7);
+    const birthdateDay = birthdate.slice(8,11);
+    const country = location.state.country;
+
+    const [fetchedData, setFetchData] = useState({});
 
     let url = 
-    `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/deaths/${BirthdateMonth}/${BirthdateDay}`;
+    `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/deaths/${birthdateMonth}/${birthdateDay}`;
 
     /* Get the results of deaths that happened on their birthday */
-    async function fetchResults() {
+    const fetchResults = async function () {
         const response = await fetch (url,
         {
             headers: {
@@ -22,17 +26,46 @@ function Loading() {
                 'Api-User-Agent': access_email
             }
         });
-    const data = await response.json()
-        .then(console.log).catch(console.error);
-}
-    fetchResults();
+        const data = await response.json();
+        setFetchData(data);
+    };
+
+    useEffect(() => {
+        fetchResults();
+    }, []);
+
+    //async function sleep() {
+      //  await new Promise(resolve => setTimeout(resolve, 5000));
+      //}
+      
+    //sleep();
+
+    const deathsArray = fetchedData.deaths;
+    console.log(deathsArray);
+
+    let pastLifeFound = false;
+    // loop over json, look for year that matches birthYear
+    //while (pastLifeFound === false) {
+        //deathsArray.forEach((death) => {
+            //console.log(`${death.year} is when your past life died`);
+            //pastLifeFound = true
+            // compare the year of death to birthdateYearInt
+            //if (death.year == death.year){
+                //pastLifeFound = true;
+            //};
+            
+
+
+        //});
+    //};
 
     return (
         <div>
             <br></br>
             <h1 className='page-title'>Your results are loading</h1>
             <br></br>
-            <p>{location.state.formattedBirthdate}</p>
+            <p>{birthdate}</p>
+            <p>{country.label}</p>
         </div>
     );
 }
