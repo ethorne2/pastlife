aa63ac7bc1c3bc947f9a6aabb31c0c852906ca33
Elizabeth Thorne <66274014+ethorne2@users.noreply.github.com>
2022-04-24 12:25:22 -0500

    add content to 404 page

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {  useNavigate } from 'react-router-dom';
 
 function NoPage() {
+
+    /* function to send user back to the homepage */
+    const navigate = useNavigate();
+    const sendtoHome = () => {
+        navigate('/');
+    };
     return (
         <div>
             <br></br>
-            <h1>Sorry the page you were looking for does not exist</h1>
+            <h1 className='page-title'>404</h1>
+            <h2 className='uncovered-text'>Sorry the page you were looking for does not exist</h2>
+            <div className='cta-button-container'>
+                <button className='cta-button' onClick={() => {sendtoHome()}}>Go to Home</button>
+            </div>
         </div>
     );
 }
