a767eb85da466815a02391c7f2d826efeb1bfa03
Elizabeth Thorne <66274014+ethorne2@users.noreply.github.com>
2022-05-20 14:27:29 -0500

    update getImage.py script

--- src/imagedownloader/getImage.py
+++ src/imagedownloader/getImage.py
@@ -4,6 +4,11 @@ def image(keyword):
     with open("keyword.txt", "w") as f:
         f.write(keyword)
 
+    completed_text = 'not completed'
+    while completed_text == 'not completed':
+        f = open("keyword.txt" , "r")
+        completed_text = f.read()
+        f.close()
 
 keyword = "reincarnation"
 image(keyword)
